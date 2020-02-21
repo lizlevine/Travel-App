b9fcbec985aa4a024e69e782cefa2f8b225b70a4
Tuscany <lizlevine.rigie@gmail.com>
2020-02-20 22:09:43 -0500

    chore: Sanity check

--- server.js
+++ server.js
@@ -3,9 +3,34 @@
 // ==============================================================================
 // this file creates express connection & runs node server
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.listen(8080, () => {
+  console.log("server is starting at port ", 8080);
+});
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
+app.use(express.static(__dirname + "/public"));
+//test
+// app.engine(
+//   'handlebars',
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+
+// var routes = require("./controllers/burgers_controller.js");
+// app.use("/", routes);
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+
+// var PORT = process.env.PORT || 8080;
+// app.listen(PORT);
